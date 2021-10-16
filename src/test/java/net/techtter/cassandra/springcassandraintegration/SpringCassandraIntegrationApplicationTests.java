0f937874af7a752ad243b8477289a460abd13c1b
surajyadav-git <64416158+surajyadav-git@users.noreply.github.com>
2021-10-16 18:00:53 +0530

    updated package

---
+++ src/test/java/net/techtter/cassandra/springcassandraintegration/SpringCassandraIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package net.techtter.cassandra.springcassandraintegration;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+class SpringCassandraIntegrationApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
