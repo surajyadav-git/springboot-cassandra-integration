b1cded55ed8aa0369468e32c270fcb03f8b7d7de
surajyadav-git <64416158+surajyadav-git@users.noreply.github.com>
2021-10-16 17:36:00 +0530

    .yml file is deleted and pom file is updated for finalname

--- src/test/java/com/example/caasandra/springcassandraintegration/SpringCassandraIntegrationApplicationTests.java
+++ src/test/java/com/example/caasandra/springcassandraintegration/SpringCassandraIntegrationApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.caasandra.springcassandraintegration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringCassandraIntegrationApplicationTests {
 
 	//@Test
