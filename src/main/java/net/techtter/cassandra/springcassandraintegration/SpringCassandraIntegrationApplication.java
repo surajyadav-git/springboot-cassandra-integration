f1500ec56451b03a42a21e05ca77659252efc86b
surajyadav-git <64416158+surajyadav-git@users.noreply.github.com>
2021-10-17 09:58:24 +0530

    123

--- src/main/java/net/techtter/cassandra/springcassandraintegration/SpringCassandraIntegrationApplication.java
+++ src/main/java/net/techtter/cassandra/springcassandraintegration/SpringCassandraIntegrationApplication.java
@@ -1,9 +1,11 @@
 package net.techtter.cassandra.springcassandraintegration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SpringCassandraIntegrationApplication {
 
 	public static void main(String[] args) {
